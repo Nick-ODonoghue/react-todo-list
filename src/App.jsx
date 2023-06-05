b3d2982fb7670878bba942b947c27b912a6f67b1
Nick ODonoghue <nicholasgodonoghue@gmail.com>
2023-06-05 16:20:25 +0100

    initial commit, site skeleton setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='grid justify-center'>
+        <h1 className='text-5xl font-bold'>Hello world!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
